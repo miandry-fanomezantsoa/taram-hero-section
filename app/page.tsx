83eea5459982da77b6d1784b69aa90c639869495
RATOVONJANAHARY Miandry Fanomezantsoa <ratovonjanahary.miandryfano@gmail.com>
2026-01-16 15:07:53 +0300

    feat: setting layout of the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
+import Bulle from "@/components/Bulle";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Créer un site web vraiment unique</h1>
+    <div className="min-h-screen bg-dark flex justify-center items-center text-white px-4 py-8">
+      <div className="flex flex-col items-center gap-8">
+        <Bulle />
+        <p>
+          Votre site doit donner envie de rester, pas de revenir en arrière.
+        </p>
+        <p>
+          Design moderne, SEO solide, Suivi complet : on construit un site qui
+          retient vos visiteurs et vous apporte des résultats.
+        </p>
 
-      <p>Votre site doit donner envie de rester, pas de revenir en arrière.</p>
-      <p>
-        Design moderne, SEO solide, Suivi complet : on construit un site qui
-        retient vos visiteurs et vous apporte des résultats.
-      </p>
-
-      <div>
-        <button>Prendre RDV</button>
-        <button>Découvrir nos projets</button>
+        <div>
+          <button>Prendre RDV</button>
+          <button>Découvrir nos projets</button>
+        </div>
       </div>
     </div>
   );
